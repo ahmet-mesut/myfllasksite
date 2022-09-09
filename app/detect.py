6b20247b01dac0358b417c4269c6b0b4466072f9
ahmet-mesut <99045664+ahmet-mesut@users.noreply.github.com>
2022-09-09 18:48:30 +0300

    new

---
+++ app/detect.py
@@ -0,0 +1,36 @@
+
+import torch
+import cv2
+
+trained_model = 'C:/Users/Murat/Desktop/herokuflask/myfllasksite/app/best.pt'
+
+#my_model2 = Licence_Plate(trained_model)
+
+
+class Licence_Plate:
+
+      def __init__(self,trained_model):
+
+            self.model = torch.hub.load('ultralytics/yolov5', 'custom', trained_model)
+
+      def detect(self,img):
+
+            self.result_model = self.model(img)
+            self.img = img
+            img = self.plot()
+
+            return img
+
+      def plot(self):
+
+            det = self.result_model.xyxy[0]
+
+            for i in range(len(det)):
+
+                pt1 = (int(det[i][0]), int(det[i][1]))
+                pt2 = (int(det[i][2]), int(det[i][3]))
+                img = cv2.rectangle(self.img,pt1,pt2,color=(0,255,0),thickness=2)
+
+            return img
+
+my_model = Licence_Plate(trained_model)
--- app/main.py
+++ app/main.py
@@ -1,8 +1,52 @@
-from flask import Flask
+from flask import Flask, render_template , request , jsonify
+from PIL import Image
+import sys
+import os
+import io
+import numpy as np
+import cv2
+import base64
+from app.detect import *
+
 
 app = Flask(__name__)
 
+
+@app.route('/detectObject',methods=['POST'])
+def mask_image():
+        file = request.files['image'].read()
+        npimg= np.fromstring(file,np.uint8)
+
+        img = cv2.imdecode(npimg,cv2.IMREAD_COLOR)
+
+        #my_model = Licence_Plate(trained_model)
+
+        img = my_model.detect(img)
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        img = Image.fromarray(img.astype("uint8"))
+        rawBytes = io.BytesIO()
+        img.save(rawBytes, "JPEG")
+
+        rawBytes.seek(0)
+
+        img_base64 = base64.b64encode(rawBytes.read())
+
+        return jsonify({'status':str(img_base64)})
+
+@app.route('/test' , methods=['GET','POST'])
+def test():
+    print("log: got at test" , file=sys.stderr)
+    return jsonify({'status':'succces'})
+
 @app.route('/')
-def index():
-    return "<h1>Hello World<h1>"
-    
+def home():
+    return render_template('./index.html')
+
+
+@app.after_request
+def after_request(response):
+    print("log: setting cors" , file = sys.stderr)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
