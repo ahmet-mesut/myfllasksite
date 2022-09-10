7a3bea9d2e10d1575d1b70b94a048be3a67d6444
ahmet-mesut <99045664+ahmet-mesut@users.noreply.github.com>
2022-09-10 23:47:16 +0300

    head

--- app/main.py
+++ app/main.py
@@ -50,3 +50,6 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
     return response
+
+if __name__ == "__main__"
+    mask_image()
